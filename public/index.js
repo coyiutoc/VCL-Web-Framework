6d4c2669ae06a76c8391a4c6de07a0d5743e3882
Tien Vu <tienv2510@microsoft.com>
2018-10-02 16:37:33 -0700

    Move dependencies to jsPsych

--- public/index.js
+++ public/index.js
@@ -1,8 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Index from "./components/Index";
+import JsPsych from './components/JsPsych';
 
-const e = <Index />;
+const e = (
+    <div>
+        <JsPsych />
+        <Index />
+    </div>
+);
 
 ReactDOM.render(
     e,
