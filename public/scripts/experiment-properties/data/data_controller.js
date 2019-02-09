b37dca299fb7f1356cd2d088d9017674d1963d7f
Zoe Zhao <zoezhao1024@gmail.com>
2019-02-09 12:30:17 -0800

    changes to data controller and constants

--- public/scripts/experiment-properties/data/data_controller.js
+++ public/scripts/experiment-properties/data/data_controller.js
@@ -1,6 +1,7 @@
 import {JND_BASE, JND_CONDITIONS} from "/scripts/experiment-properties/data/constants/jnd_data.js";
 import {STEVENS_BASE, STEVENS_CONDITIONS} from "/scripts/experiment-properties/data/constants/stevens_data.js";
 import {JND_RADIUS_BASE} from "/scripts/experiment-properties/data/constants/jnd_radius_data.js";
+import {ESTIMATION_BASE, ESTIMATION_CONDITIONS} from "./constants/estimation_data";
 
 export { get_data,
          get_data_subset };
@@ -61,6 +62,15 @@ function get_data(experiment){
       dataset = create_condition_dataset(dataset, STEVENS_CONDITIONS[condition]);
     }
   }
+  else if (experiment_name === "Estimation") {
+    dataset = ESTIMATION_BASE[range];
+      if (condition !== "base") {
+        if (!ESTIMATION_CONDITIONS[condition]) {
+            throw new Error(condition + " not supported.");
+        }
+        dataset = create_condition_dataset(dataset, ESTIMATION_CONDITIONS[condition]);
+      }
+  }
   else {
     throw new Error(experiment + " not supported.");
   }
@@ -100,7 +110,7 @@ function get_data_subset(experiment, range, condition) {
  * Appends condition-specific data to the dataset.
  *
  * @param  dataset           [{assoc}, {assoc}, .... ]     dataset with base experiment constants   
- *         condition_data    [{assoc}, {assoc}, .... ]     condition set for that experiment         
+ * @param  condition_data    [{assoc}, {assoc}, .... ]     condition set for that experiment
  *
  * @return dataset           [{assoc}, {assoc}, .... ]  
  **/
