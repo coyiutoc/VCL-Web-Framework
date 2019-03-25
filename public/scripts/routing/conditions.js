d600a1c4f44edbf835d7967dddd3947a0066a7da
Caitlin Coyiuto <coyiutoc@gmail.com>
2019-03-25 00:02:41 -0700

    Starting refactor on UI. Dynamic filtering on small dataset.

---
+++ public/scripts/routing/conditions.js
@@ -0,0 +1,28 @@
+var CONDITIONS = {
+
+	base: {
+		experiment: ["jnd", "stevens"],
+		graph_type: ["scatter", "strip"],
+		trial_structure: "foundational",
+		balancing: "latin_square",
+		display_name: "Base"
+	},
+
+	strip_ring_size: {
+		experiment: ["jnd", "stevens"],
+		graph_type: ["ring"],
+		trial_structure: ["foundational"],
+		balancing: ["latin_square"],
+		display_name: "Strip Ring Size"
+	},
+
+	line_length_strip: {
+		experiment: ["jnd", "stevens"],
+		graph_type: ["strip"],
+		trial_structure: ["foundational"],
+		balancing: ["latin_square"],
+		display_name: "Line Length Strip"
+	}
+
+};
+
