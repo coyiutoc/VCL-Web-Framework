b4c24dde8e91cb8e3d0d82f06b0dcf7c906ce80d
Caitlin Coyiuto <coyiutoc@gmail.com>
2019-04-16 20:55:48 -0700

    Added balancing config.

---
+++ public/config/balancing-config.js
@@ -0,0 +1,16 @@
+var BALANCING_TYPES = {
+
+	latin_square: {
+		docs : {
+			display_name: "Latin Square",
+			description: "Uses an n x n matrix to balance subconditions presentation."
+		}
+	},
+
+	random: {
+		docs: {
+			display_name: "Random",
+			description: "Randomizes the order of the subcondition presentation."
+		}
+	}
+};
